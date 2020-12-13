8f610f6b2c9873a6a662414c398c4dc13a89fc68
Esau, Nathan <Nathan.Esau@nielsen.com>
2020-12-12 23:04:29 -0500

    day 10

--- solutions/day10/solution.py
+++ solutions/day10/solution.py
@@ -1,1 +1,24 @@
-# TODO
+def solution_1(arr):
+    arr = sorted(arr)
+    prev_adapter = 0
+    count = {1:0, 3:0}
+    for adapter in arr:
+        diff = adapter - prev_adapter
+        count[diff] += 1
+        prev_adapter = adapter
+    count[3] += 1 # last item
+    return count[1] * count[3]
+
+
+def solution_2(arr):
+    arr = sorted(arr)
+    partial = [0 for i in range(len(arr))]
+    partial[0] = 1
+    for i in range(1, len(arr)):
+        count = 0
+        j = i - 1
+        while j >= 0 and arr[i] - arr[j] <= 3:
+            count += partial[j] # all paths before and including arr[j]
+            j -= 1
+        partial[i] = count
+    return partial[-1]
---
+++ solutions/day10/test_solution.py
@@ -0,0 +1,66 @@
+import os
+from .solution import (
+    solution_1,
+    solution_2,
+    solution_2_alternate
+)
+
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def read_input():
+    with open("{}/assets/input.txt".format(BASEDIR)) as f:
+        data = f.read()
+    return data
+
+
+def test_solution_1_example():
+    """
+    provided example
+    """
+    data = """
+    16
+    10
+    15
+    5
+    1
+    11
+    7
+    19
+    6
+    12
+    4
+    """
+    arr = [int(line.strip()) for line in data.splitlines() if line.strip()]
+    s1 = solution_1(arr)
+    assert s1 == 35
+
+
+def test_solution_1():
+    """
+    use input.txt
+    """
+    data = read_input()
+    arr = [int(line.strip()) for line in data.splitlines() if line.strip()]
+    s1 = solution_1(arr)
+    assert s1 == 2232
+
+
+def test_solution_2_example():
+    """
+    provided example
+    """
+    arr = [1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19]
+    s1 = solution_2(arr)
+    assert s1 == 8
+
+
+def test_solution_2():
+    """
+    use input.txt
+    """
+    data = read_input()
+    arr = [int(line.strip()) for line in data.splitlines() if line.strip()]
+    s2 = solution_2(arr)
+    assert s2 == 86812553324672
